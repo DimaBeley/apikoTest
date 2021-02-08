a44329fa2463a2584521b3474547d1840e47819a
DimaBeley <2beyva@gmail.com>
2021-02-08 20:53:14 +0000

    add vue cli with router and vuex

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    <div class="login">login</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Login"
+  }
+</script>
+
+<style>
+  .login {
+      border: 1px solid black;
+    }
+</style>
